a16f5412cb1916d3b09b299129e51cdc2592a646
Lazzat Mukhametalinova <2248lm@gmail.com>
2022-01-28 17:38:13 +0600

    feat: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let confArr = bracketsConfig.map(el => el.join(""));
+  let checkStr = (s) => {
+    for (let k of confArr) {
+      if (s.includes(k)) s = s.replace(k, "");
+    }
+    str = s;
+    for (let k of confArr) {
+      if (str.includes(k)) checkStr(str);
+    }
+  }
+  checkStr(str);
+  return str.length === 0;
 }
